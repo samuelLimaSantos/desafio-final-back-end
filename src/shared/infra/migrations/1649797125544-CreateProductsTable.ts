806eca61764b863838de3d2b82b4375fc806bb47
Samuel Santos <samuellima280499@gmail.com>
2022-04-12 18:05:02 -0300

    Adicionando a migrations de criação da tabela de products

---
+++ src/shared/infra/migrations/1649797125544-CreateProductsTable.ts
@@ -0,0 +1,61 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateProductsTable1649797125544 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'products',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                },
+                {
+                    name: 'id_restaurant',
+                    type: 'uuid',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                    isUnique: true
+                },
+                {
+                    name: 'url_image',
+                    type: 'varchar',
+                },
+                {
+                    name: 'description',
+                    type: 'varchar',
+                },
+                {
+                    name: 'price',
+                    type: 'decimal',
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                }
+            ]
+        }));
+
+        await queryRunner.createForeignKey('products', new TableForeignKey({
+            name: 'fk_products_restaurants',
+            columnNames: ['id_restaurant'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'restaurants',
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('products', 'fk_products_restaurants');
+        await queryRunner.dropTable('products');
+    }
+
+}
