612dbda6654cc43585f59fe5afdba1ccdb16e25f
Samuel Santos <samuellima280499@gmail.com>
2022-04-12 16:49:35 -0300

    Adicionando configuração básica do swagger e validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('rest');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle(`JASON's food`)
+    .setDescription(`Documentation for the API of JASON\'s food`)
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
