5966a8c250e327570b32d98c35764834189c0d9a
Samuel Santos <samuellima280499@gmail.com>
2022-04-12 17:58:10 -0300

    Adicionando configuração das migrations

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : null,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  migrations: [__dirname + '/src/shared/infra/migrations/**/*{.ts,.js}'],
+  migrationsRun: false,
+  cli: {
+    entitiesDir: 'src',
+    migrationsDir: 'src/shared/infra/migrations',
+  },
+}
