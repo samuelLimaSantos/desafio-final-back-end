38f395bb0504b8532a6c9038cca0229e43a586f1
Samuel Santos <samuellima280499@gmail.com>
2022-04-12 22:21:39 -0300

    Subindo arquivo de constantes

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,161 @@
+import { DeepPartial } from 'typeorm';
+import { Extras } from './../../modules/products/entities/extras.entity';
+
+export const EXTRAS_OBJECT: DeepPartial<Extras[]> = [
+  {
+    "price": 33.64,
+    "name": "Roberts"
+  },
+  {
+    "price": 1.54,
+    "name": "Olga"
+  },
+  {
+    "price": 65.61,
+    "name": "Deanne"
+  },
+  {
+    "price": 11.84,
+    "name": "Hudson"
+  },
+  {
+    "price": 54.1,
+    "name": "Vincent"
+  },
+  {
+    "price": 76.15,
+    "name": "Logan"
+  },
+  {
+    "price": 12.55,
+    "name": "Bernard"
+  },
+  {
+    "price": 29.94,
+    "name": "Bradford"
+  },
+  {
+    "price": 94.49,
+    "name": "Johanna"
+  },
+  {
+    "price": 22.48,
+    "name": "Ginger"
+  },
+  {
+    "price": 89.07,
+    "name": "Sonja"
+  },
+  {
+    "price": 87.71,
+    "name": "Mcneil"
+  },
+  {
+    "price": 15.36,
+    "name": "Armstrong"
+  },
+  {
+    "price": 23.33,
+    "name": "Saundra"
+  },
+  {
+    "price": 10.85,
+    "name": "Casandra"
+  },
+  {
+    "price": 84.99,
+    "name": "Santana"
+  },
+  {
+    "price": 77.49,
+    "name": "Hicks"
+  },
+  {
+    "price": 10.6,
+    "name": "Abbott"
+  },
+  {
+    "price": 60.63,
+    "name": "Moses"
+  },
+  {
+    "price": 52.88,
+    "name": "Miranda"
+  },
+  {
+    "price": 91.9,
+    "name": "Liz"
+  },
+  {
+    "price": 21.48,
+    "name": "Hattie"
+  },
+  {
+    "price": 24.58,
+    "name": "Benita"
+  },
+  {
+    "price": 71.04,
+    "name": "Spencer"
+  },
+  {
+    "price": 82.14,
+    "name": "Melanie"
+  },
+  {
+    "price": 9.79,
+    "name": "Ann"
+  },
+  {
+    "price": 79.82,
+    "name": "Hoover"
+  },
+  {
+    "price": 6.7,
+    "name": "Gloria"
+  },
+  {
+    "price": 67.27,
+    "name": "Alexandra"
+  },
+  {
+    "price": 82.71,
+    "name": "Vega"
+  },
+  {
+    "price": 31.33,
+    "name": "Ivy"
+  },
+  {
+    "price": 4.77,
+    "name": "Wilkins"
+  },
+  {
+    "price": 60.11,
+    "name": "Ellen"
+  },
+  {
+    "price": 14.95,
+    "name": "Golden"
+  },
+  {
+    "price": 33.07,
+    "name": "Danielle"
+  },
+  {
+    "price": 66.14,
+    "name": "Eleanor"
+  },
+  {
+    "price": 45.14,
+    "name": "Griffith"
+  },
+  {
+    "price": 27.04,
+    "name": "Drake"
+  },
+  {
+    "price": 95.54,
+    "name": "Jensen"
+  },
+]
