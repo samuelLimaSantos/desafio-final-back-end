23a3cc845714ece412dcd37c55bfd160535f005a
Samuel Santos <samuellima280499@gmail.com>
2022-04-13 00:27:37 -0300

    Implementando endpoint de listar restaurantes

--- src/modules/restaurants/useCases/listRestaurants/listRestaurants.service.ts
+++ src/modules/restaurants/useCases/listRestaurants/listRestaurants.service.ts
@@ -1,8 +1,18 @@
+import { Repository } from 'typeorm';
 import { Injectable } from "@nestjs/common";
+import { Restaurants } from '../../entities/restaurants.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ListRestaurantsService {
-  constructor() { }
+  constructor(
+    @InjectRepository(Restaurants)
+    private restaurantsRepository: Repository<Restaurants>
+  ) { }
 
-  async execute() {}
+  async execute() {
+    return await this.restaurantsRepository.find({
+      select: ['id', 'name', 'address', 'urlLogo'],
+    });
+  }
 }
